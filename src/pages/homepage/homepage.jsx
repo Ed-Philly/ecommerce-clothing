dff4842ce54920c6983c214227392178ed2bc395
Ed philly <osas.edok@yahoo.co.uk>
2021-01-13 22:45:14 +0000

    homepage broken down into componets - menu items, directory, homepage componets

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import DirectoryMenu from '../../components/directory/directory';
+import "./homepage.scss"
+
+
+const HomePage = () => {
+    return ( 
+        <div className="homepage">
+            <DirectoryMenu/>
+        </div>
+     );
+}
+ 
+export default HomePage;
