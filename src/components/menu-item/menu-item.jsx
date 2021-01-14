34f3f89924b3b79730883af6c4f2cdddc2b13417
Ed philly <osas.edok@yahoo.co.uk>
2021-01-14 03:30:30 +0000

    update our menu-item component and App with react Router

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useHistory ,useRouteMatch} from 'react-router-dom';
 import './menu-item.scss'
 
-const MenuItem = ({title,imageUrl,size}) => {
+const MenuItem = ({title,imageUrl,size,linkUrl}) => {
+
+    let history = useHistory();
+    let match = useRouteMatch();
+
+    function handleClick(){
+        history.push(`${match.url}${linkUrl}`)
+    }
     return (
-        <div className ={`${size} menu-item`} >
+        <div className ={`${size} menu-item`} onClick={handleClick}>
 
             <div 
                 className="background-image" 
